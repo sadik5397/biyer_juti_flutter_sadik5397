50025962e1519cb6a7e3eb39b84fcdb177a886ff
sadik5397@gmail.com </student/>
2023-10-27 23:13:33 +0600

    Added Theme Function

---
+++ lib/util/global_function.dart
@@ -0,0 +1,7 @@
+initialConfiguration() {
+  // ignore: avoid_print
+  print("---- Biyer Juti | User App ----\n---- "
+      "© Developed by : S.a. Sadik | DM Bikash ----\n---- "
+      "sadik5397@gmail.com | bikash.workplace@gmail.com ----\n---- "
+      "https://github.com/sadik5397 | https://github.com/dmbikash ----");
+}
