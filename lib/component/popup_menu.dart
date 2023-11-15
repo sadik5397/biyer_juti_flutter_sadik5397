097dd3d89c4b34c08092a85a59aa7ce7e3f93cf8
sadik5397 <sadik5397@gmail.com>
2023-11-15 18:26:00 +0600

    Added Footer Menu

--- lib/component/popup_menu.dart
+++ lib/component/popup_menu.dart
@@ -6,27 +6,28 @@ import 'package:flutter/material.dart';
 import '../theme/colors.dart';
 import '../theme/gap.dart';
 
-PopupMenuItem menuItem({required String label, required IconData icon, VoidCallback? onTap}) => PopupMenuItem(
-    // onTap: onTap,
-    value: 0,
+PopupMenuItem menuItem({required String label, required IconData icon, VoidCallback? onTap, required int index}) => PopupMenuItem(
+    value: index,
     padding: ThemePadding.px1,
-    child: Hyperlink(
-        onTap: onTap ?? () {},
-        borderRadius: ThemeBorderRadius.r1,
-        child: Padding(padding: ThemePadding.p4, child: Row(children: [Icon(icon, size: 16), Gap.gx3, Text(label, style: TextStyle(fontWeight: FontWeight.bold, color: ThemeColor.navyBlue))]))));
+    child: Hyperlink(onTap: onTap ?? () {}, borderRadius: ThemeBorderRadius.r1, child: Padding(padding: ThemePadding.p4, child: Row(children: [Icon(icon, size: 16), Gap.gx3, Text(label, style: TextStyle(fontWeight: FontWeight.bold, color: ThemeColor.navyBlue))]))));
 
-// class MenuItem extends StatelessWidget {
-//   const MenuItem({super.key, required this.label, required this.icon, this.to});
-//
-//   final String label;
-//   final IconData icon;
-//   final Widget? to;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return PopupMenuItem(
-//       child: Row(children: [Icon(icon, size: 16), Gap.gx2, Text(label, style: TextStyle(fontWeight: FontWeight.bold, color: ThemeColor.navyBlue))]),
-//       onTap: () => route(context, to ?? const Home()),
-//     );
-//   }
-// }
+PopupMenuItem footerItem({required VoidCallback toAbout, required VoidCallback toTnC, required VoidCallback toPrivacy, required VoidCallback toARefund, required int index}) => PopupMenuItem(
+    value: index,
+    padding: ThemePadding.p4,
+    child: Column(children: [
+      Row(children: [
+        Hyperlink(onTap: toAbout, borderRadius: ThemeBorderRadius.r1, child: const Text("About Us", style: TextStyle(fontSize: 12, color: Colors.black45))),
+        Gap.gx3,
+        const Icon(Icons.circle, color: Colors.black45, size: 8),
+        Gap.gx3,
+        Hyperlink(onTap: toTnC, borderRadius: ThemeBorderRadius.r1, child: const Text("Terms & Conditions", style: TextStyle(fontSize: 12, color: Colors.black45)))
+      ]),
+      Gap.gy2,
+      Row(children: [
+        Hyperlink(onTap: toPrivacy, borderRadius: ThemeBorderRadius.r1, child: const Text("Privacy Policy", style: TextStyle(fontSize: 12, color: Colors.black45))),
+        Gap.gx3,
+        const Icon(Icons.circle, color: Colors.black45, size: 8),
+        Gap.gx3,
+        Hyperlink(onTap: toARefund, borderRadius: ThemeBorderRadius.r1, child: const Text("Refund Policy", style: TextStyle(fontSize: 12, color: Colors.black45)))
+      ])
+    ]));
--- lib/util/menu.dart
+++ lib/util/menu.dart
@@ -14,16 +14,17 @@ import '../views/online.dart';
 import '../views/profile_viewer.dart';
 
 List<PopupMenuEntry> appMenu(BuildContext context) => [
-      menuItem(label: "My Profile", icon: FeatherIcons.user, onTap: () => route(context, const Profile(myProfile: true))),
-      menuItem(label: "Notification", icon: FeatherIcons.bell, onTap: () => route(context, const Notifications())),
-      menuItem(label: "Search", icon: FeatherIcons.search, onTap: () => route(context, const Search())),
-      menuItem(label: "Hearts Received", icon: FeatherIcons.heart, onTap: () => route(context, const Home())),
-      menuItem(label: "Matches", icon: FeatherIcons.gitPullRequest, onTap: () => route(context, const Match())),
-      menuItem(label: "Profile Viewers", icon: FeatherIcons.eye, onTap: () => route(context, const ProfileViewer())),
-      menuItem(label: "Online Users", icon: FeatherIcons.users, onTap: () => route(context, const Online())),
-      menuItem(label: "Choose Viewers", icon: FeatherIcons.mousePointer),
-      menuItem(label: "Billing", icon: FeatherIcons.creditCard, onTap: () => route(context, const UpgradeToPremium())),
-      menuItem(label: "Settings", icon: FeatherIcons.settings),
-      menuItem(label: "Contact Us", icon: FeatherIcons.phone),
-      menuItem(label: "Logout", icon: FeatherIcons.logOut, onTap: () => routeNoBack(context, const SignIn())),
+      menuItem(index: 0, label: "My Profile", icon: FeatherIcons.user, onTap: () => route(context, const Profile(myProfile: true))),
+      menuItem(index: 1, label: "Notification", icon: FeatherIcons.bell, onTap: () => route(context, const Notifications())),
+      menuItem(index: 2, label: "Search", icon: FeatherIcons.search, onTap: () => route(context, const Search())),
+      menuItem(index: 3, label: "Hearts Received", icon: FeatherIcons.heart, onTap: () => route(context, const Home())),
+      menuItem(index: 4, label: "Matches", icon: FeatherIcons.gitPullRequest, onTap: () => route(context, const Match())),
+      menuItem(index: 5, label: "Profile Viewers", icon: FeatherIcons.eye, onTap: () => route(context, const ProfileViewer())),
+      menuItem(index: 6, label: "Online Users", icon: FeatherIcons.users, onTap: () => route(context, const Online())),
+      menuItem(index: 7, label: "Choose Viewers", icon: FeatherIcons.mousePointer),
+      menuItem(index: 8, label: "Billing", icon: FeatherIcons.creditCard, onTap: () => route(context, const UpgradeToPremium())),
+      menuItem(index: 9, label: "Settings", icon: FeatherIcons.settings),
+      menuItem(index: 10, label: "Contact Us", icon: FeatherIcons.phone),
+      menuItem(index: 11, label: "Logout", icon: FeatherIcons.logOut, onTap: () => routeNoBack(context, const SignIn())),
+      footerItem(index: 12, toAbout: () {}, toTnC: () {}, toPrivacy: () {}, toARefund: () {})
     ];
